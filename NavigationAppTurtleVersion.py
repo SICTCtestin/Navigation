d467fc41fa8b26ce5ffbb08fde580d9559844917
padawan <mason.camp@evsck12.com>
2021-09-14 14:06:45 -0500

    Turtle movement

--- NavigationAppTurtleVersion.py
+++ NavigationAppTurtleVersion.py
@@ -1,3 +1,24 @@
 from turtle import *
 import turtle
+def lookUp():
+    navigator.setheading(90)
+def lookDown():
+    navigator.setheading(270)
+def lookRight():
+    navigator.setheading(0)
+def lookLeft():
+    navigator.setheading(180)
+def go():
+    navigator.forward(20)
+navigator=turtle.Turtle()
+navigator.shape("circle")
 wn=turtle.Screen()
+
+wn.onkeypress(lookUp,"Up")
+wn.onkeypress(lookDown,"Down")
+wn.onkeypress(lookLeft,"Left")
+wn.onkeypress(lookRight,"Right")
+wn.onkeypress(go,"g")
+wn.listen()
+
+wn.mainloop()
